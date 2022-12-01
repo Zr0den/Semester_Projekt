69c2b291eef4dcb409f7a0383508201f75a7beb5
MartinSThomsen <martinbastrup01@outlook.dk>
2022-12-01 12:03:41 +0100

    Projekt Demo

---
+++ Projekt.Domain/ProjektModel/ProjektEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.Domain.ProjektModel
+{
+    public class ProjektEntity
+    {
+        public int ProjektID { get; }
+        public string ProjektName { get; private set; }
+        public int SælgerID { get; set; }
+        public DateTime EstSlutDato { get; set; }
+        public virtual ICollection<OpgaveEntity> Opgaver { get; private set; }
+
+        internal ProjektEntity()
+        {
+
+        }
+
+        public ProjektEntity(
+            string projektName, 
+            ICollection<OpgaveEntity> opgaver)
+        {
+            this.Opgaver = new HashSet<OpgaveEntity>();
+            ProjektName = projektName;
+        }
+
+        public void Edit(string projektName, ICollection<OpgaveEntity> Opgaver)
+        {
+            ProjektName = projektName;
+            Opgaver = opgaver;
+        }
+    }
+}
